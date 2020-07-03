a829af5939d589e4585c82a3127fd93d5d51f11b
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-03 17:16:01 +0300

    added class generic list generic with methods Integer/String generics

---
+++ src/main/java/MyListModify.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyListModify<T extends Comparable<? super T>> {
+    List<T> list = new ArrayList<T>();
+
+    public void add(T t) {
+        list.add(t);
+    }
+
+    public T printLargest() {
+        return Collections.max(list);
+    }
+
+    public T printSmallest() {
+        return Collections.min(list);
+    }
+}
